3d606f17ad106f5c5ab22b1709971d5c3119bed0
devDeveloperMohsin <mohsin.ali8541@gmail.com>
2021-08-13 00:10:28 +0500

    Navbar Design Done

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <nav id="navbar">
+      <div class="container">
+        <div class="navbar-content">
+          <div class="logo-wrapper">
+            <img src="@/assets/images/logo.png" alt="taggz logo" class="logo">
+          </div>
+          <div class="links-wrapper">
+            <a href="" class="navbar-link youtube">
+              <img src="@/assets/images/youtube-32.png" alt="youtube">
+              Youtube
+            </a>
+            <a href="" class="navbar-link twitter">
+              <img src="@/assets/images/twitter-32.png" alt="youtube">
+              Twitter
+            </a>
+            <a href="" class="navbar-link tiktok">
+              <img src="@/assets/images/tiktok-32.png" alt="youtube">
+              Tiktok
+            </a>
+          </div>
+        </div>
+      </div>
+    </nav>
+
+    <div id="main">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+}
+</script>
